4f3e55b34cf9f3ac8b2d95b3ca542fa813514c43
piotrwojdan <piotr.wojdan2001@gmail.com>
2022-12-19 16:12:29 +0100

    Initial Commit

---
+++ Shop-Lap/Shop_LapApp.swift
@@ -0,0 +1,17 @@
+//
+//  Shop_LapApp.swift
+//  Shop-Lap
+//
+//  Created by Piotr Wojdan on 16/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct Shop_LapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
